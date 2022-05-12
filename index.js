941ef5118756b3e123aa8865e613c3c49e248a37
Binamra Lamsal <61900781+binamralamsal@users.noreply.github.com>
2022-05-12 20:01:57 +0545

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ import connectDB from "./config/db.js";
 // Connect Databases
 connectDB();
 
+// Express setup
+import express from 'express'
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.get("/", async (req, res) => {
+  res.json({ Hello: "World" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
 // Commands
 import "./commands/channel.js";
 import "./commands/utils.js";
